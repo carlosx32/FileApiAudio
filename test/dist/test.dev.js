2eebffda965e69f06432a7f90b8be9ea46eb9306
carlos camacho <carlos0camacho@gmail.com>
2020-08-29 23:48:35 -0500

    test file

--- test/dist/test.dev.js
+++ test/dist/test.dev.js
@@ -1,4 +1,7 @@
+"use strict";
+
 var assert = require('assert');
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
